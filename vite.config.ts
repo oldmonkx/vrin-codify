f54c939b85954ef9e215d912cc505d09582fda2d
oldmonkx <mayankx0101@gmail.com>
2026-06-08 12:03:48 +0530

    Fix image optimizer SVG config

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,6 @@ export default defineConfig(({mode}) => {
               params: {
                 overrides: {
                   cleanupIds: false,
-                  removeViewBox: false,
                 },
               },
             },
